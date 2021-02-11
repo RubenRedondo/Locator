f45c8968d8e2a1be957b70d2b23dda5b7a3cd7ee
Ruben Redondo <ruben.redondob@icloud.com>
2021-02-10 19:40:14 -0600

    Hardcoded auth

--- app/src/main/java/com/example/locator/core/LoginController.kt
+++ app/src/main/java/com/example/locator/core/LoginController.kt
@@ -1,5 +1,6 @@
 package com.example.locator.core
 
+//Auth methods would be here
 
     fun login (username: String, password: String) : Boolean {
         return username.equals("miusuario") && password.equals("secret_key")
