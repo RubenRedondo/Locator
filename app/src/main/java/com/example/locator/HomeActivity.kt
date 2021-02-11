cf2ded4d0e1c9b804a3180e1b69928d786d50d04
Ruben Redondo <ruben.redondob@icloud.com>
2021-02-10 19:38:08 -0600

    Get location added

--- app/src/main/java/com/example/locator/HomeActivity.kt
+++ app/src/main/java/com/example/locator/HomeActivity.kt
@@ -1,4 +1,116 @@
 package com.example.locator
 
-class HomeActivity {
+import android.Manifest
+import android.R
+import android.content.Context
+import android.content.Intent
+import android.content.pm.ActivityInfo
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.os.Bundle
+import android.os.Looper
+import android.widget.Button
+import android.widget.EditText
+import androidx.annotation.Nullable
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.example.locator.core.login
+import com.google.android.gms.location.*
+
+
+class HomeActivity : AppCompatActivity() {
+
+
+    lateinit var dialog: CustomDialog
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    lateinit var locationRequest: LocationRequest
+    val PERMISSION_ID = 1010
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(com.example.locator.R.layout.activity_home)
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        val locationbtn = findViewById<Button>(com.example.locator.R.id.locationbtn)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        locationbtn.setOnClickListener {
+            requestPermission()
+            getLastLocation()
+        }
+    }
+
+
+
+
+    fun checkPermission():Boolean{
+        return ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    fun requestPermission(){
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf( android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION),
+            PERMISSION_ID
+        )
+    }
+
+    fun isLocationEnabled():Boolean{
+        var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    fun getLastLocation(){
+        if(checkPermission()){
+            if(isLocationEnabled()){
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener {task->
+                    var location: Location? = task.result
+                    if(location == null){
+                        newLocation()
+                    }else{
+                        setDialog("Your location...", location.latitude, location.longitude)
+                    }
+                }
+            }else{
+                setDialog("Error", "Please turn on your device location.", "")
+            }
+        }else{
+            requestPermission()
+        }
+    }
+
+    fun newLocation(){
+        var locationRequest =  LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 0
+        locationRequest.fastestInterval = 0
+        locationRequest.numUpdates = 1
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        fusedLocationProviderClient!!.requestLocationUpdates(
+            locationRequest, locationCallback, Looper.myLooper()
+        )
+    }
+
+    private val locationCallback = object : LocationCallback(){
+        override fun onLocationResult(locationResult: LocationResult) {
+            var lastLocation: Location = locationResult.lastLocation
+            setDialog("Your location...",lastLocation.latitude, lastLocation.longitude)
+        }
+    }
+
+    fun setDialog (title: String, text1 : Any, text2: Any){
+        dialog = CustomDialog(this, "Your location...", "Latitude: ${text1}", "Longitude: ${text2}")
+        dialog.showDialog()
+    }
+
 }
